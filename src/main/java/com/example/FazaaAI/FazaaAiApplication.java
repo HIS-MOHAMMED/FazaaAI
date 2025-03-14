a9efc56add6b241d008dffda420d5fd3bb16cd20
Hisham Mohammed <hishammohammed1199@gmail.com>
2025-03-15 00:15:07 +0300

    initial commit

---
+++ src/main/java/com/example/FazaaAI/FazaaAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FazaaAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FazaaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FazaaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FazaaAI/FazaaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FazaaAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FazaaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
